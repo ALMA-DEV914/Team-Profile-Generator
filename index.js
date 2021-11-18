70296720b8db096e851a70a3a665a9ccae450fbb
ALMA_DEV914 <aungonalna58@gamil.com>
2021-11-18 13:56:07 -0800

    Add page template

---
+++ index.js
@@ -0,0 +1,156 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+const generatePage = require('./src/pageTemplate')
+const myTeam = [];
+
+const managerInfo = () => {
+    return inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the manager's name?",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "What is the manager's id?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "What is the manager's email?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What is the manager's office number?",
+                name: "office",
+            },
+        ])
+        .then(response => {
+                const manager = new Manager(response.name, response.id, response.email, response.office);
+                myTeam.push(manager);
+        })
+    };
+
+
+const teamInfo = ()=> {
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Choose employee job title",
+            name: "role",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Finished adding employees"
+            ]
+        },
+
+        {
+            message: "What is name of engineer?",
+            name: "name",
+            when: ({role}) => role === "Engineer"
+        },
+
+        {
+            message: "What is the engineer's ID?",
+            name: "id",
+            when: ({role}) => role === "Engineer"
+        },
+
+        {
+            message: "What is the engineer's email address?",
+            name: "email",
+            when: ({role}) => role === "Engineer"
+        },
+
+        {
+            message: "what is the engineer's GitHub username?",
+            name: "github",
+            when: ({role}) => role === "Engineer"
+        },
+
+        {
+            message: "What is the intern's name?",
+            name: "name",
+            when: ({role}) => role === "Intern"
+        },
+
+        {
+            message: "What is the intern's ID?",
+            name: "id",
+            when: ({role}) => role === "Intern"
+        },
+
+        {
+            message: "What is the intern's email address?",
+            name: "email",
+            when: ({role}) => role === "Intern"
+        },
+
+        {
+            message: "What is the intern's school?",
+            name: "school",
+            when: ({role}) => role === "Intern"
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddMember',
+            message: 'Would you like to add another team member?',
+            default: false
+        }
+    ])
+    .then(response => {
+
+        switch (response.role) {
+            case "Engineer":
+                const engineer = new Engineer(response.name, response.id, response.email, response.github);
+                myTeam.push(engineer);
+                break;
+            case "Intern":
+                const intern = new Intern(response.name, response.id, response.email, response.school);
+                myTeam.push(intern);
+                break;
+        }
+        if (response.confirmAddMember){
+            return teamInfo()
+        }
+
+    })
+}
+
+managerInfo()
+.then(teamInfo)
+.then(() => generatePage(myTeam))
+
+.then(pageHTML => {
+    console.log(myTeam)
+    return writeFile(pageHTML);
+  })
+.catch(err => {
+    console.log(err);
+  });
+
+
+const writeFile = fileContent =>{
+    return new Promise((resolve, reject)=>{
+        fs.writeFile('./dist/index.html', fileContent, err =>{
+            if(err){
+                reject(err)
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        })
+    });
+};
+
+
+
+
---
+++ src/pageTemplate.js
@@ -0,0 +1,109 @@
+const Employee = require("../lib/Employee")
+const Engineer = require("../lib/Engineer")
+const Manager = require("../lib/Manager")
+const Intern = require("../lib/Intern")
+let teamMembers = '';
+
+const engineerHTML = (member) => {
+    return `
+    <div class="card">
+      <div class="card-header bg-primary">
+          <h2 class="card-title">${member.name }</h2>
+          <h3 class="card-title">${member.getRole()}</h3>
+      </div>
+      <div class="card-body">
+          <ul class="list-group">
+              <li class="list-group-item">ID: ${member.id}</li>
+              <li class="list-group-item">Email: <a href="mailto:${member.email}">${member.email}</a></li>
+              <li class="list-group-item">GitHub: <a href="https://github.com/${member.github}" target="_blank" rel="noopener noreferrer">github</a></li>
+          </ul>
+      </div>
+    </div>
+      
+    `;
+};
+
+const internHTML = (member) => {
+  return `
+    <div class="card employee-card">
+      <div class="card-header bg-primary">
+          <h2 class="card-title">${member.name }</h2>
+          <h3 class="card-title">${member.getRole()}</h3>
+      </div>
+      <div class="card-body">
+          <ul class="list-group">
+              <li class="list-group-item">ID: ${member.id}</li>
+              <li class="list-group-item">Email: <a href="mailto:${member.email}">${member.email}</a></li>
+              <li class="list-group-item">School: ${member.school}</li>
+          </ul>
+      </div>
+    </div>
+      
+    `;
+};
+const managerHTML = (member) => {
+    return `
+      <div class="card employee-card">
+        <div class="card-header bg-primary">
+            <h2 class="card-title">${member.name }</h2>
+            <h3 class="card-title">${member.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${member.id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${member.email}">${member.email}</a></li>
+                <li class="list-group-item">Office: ${member.office}</li>
+            </ul>
+        </div>
+      </div>
+        
+      `;
+  };
+  
+  const generateHTML= (myTeam) =>{
+      myTeam.forEach(member => {
+          if(member.getRole() === 'Engineer'){
+              let engineer = engineerHTML(member)
+              teamMembers += engineer
+      } else if(member.getRole() === 'Intern'){
+          let intern =   internHTML(member)
+          teamMembers += intern
+      } else{
+          let manager = managerHTML(member)
+          teamMembers += manager
+      }
+  } 
+      )
+    
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Portfolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    </head>
+    
+    <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">My Team</h1>
+      </div>
+    </header>
+    <main class="container my-5">
+        
+    ${teamMembers}
+        
+        
+    </main>
+  </body>
+    </html>
+    `;
+}
+
+
+module.exports = generateHTML;
