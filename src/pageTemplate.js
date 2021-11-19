f45fe8547e996c1201f73e26a610687f7bff28a0
ALMA_DEV914 <aungonalna58@gamil.com>
2021-11-18 20:40:21 -0800

    Add style.css file

--- src/pageTemplate.js
+++ src/pageTemplate.js
@@ -8,10 +8,10 @@ let teamMembers = '';
 const engineerHTML = (member) => {
     return `
     <div class="column col-3 my-5">
-    <div class="card text-center mx-3 bg-dark">
-      <div class="card-header bg-primary my-4 mx-2">
+    <div class="card text-center mx-3">
+      <div class="card-header my-4 mx-2">
           <h2 class="card-title text-light">${member.name }</h2>
-          <h3 class="card-title">${member.getRole()}</h3>
+          <h3 class="card-title" id="engineer">${member.getRole()}</h3>
       </div>
       <div class="card-body">
           <ul class="list-group">
@@ -28,10 +28,10 @@ const engineerHTML = (member) => {
 const internHTML = (member) => {
   return `
   <div class="column col-3 my-5">
-    <div class="card text-center mx-3 bg-dark">
-      <div class="card-header bg-primary my-4 mx-2">
+    <div class="card text-center mx-3">
+      <div class="card-header my-4 mx-2">
           <h2 class="card-title text-light">${member.name }</h2>
-          <h3 class="card-title">${member.getRole()}</h3>
+          <h3 class="card-title" id="intern">${member.getRole()}</h3>
       </div>
       <div class="card-body">
           <ul class="list-group">
@@ -47,10 +47,10 @@ const internHTML = (member) => {
 const managerHTML = (member) => {
     return `
     <div class="column col-3 my-5">
-      <div class="card text-center mx-3 bg-dark">
-        <div class="card-header bg-primary my-4 mx-2">
+      <div class="card text-center mx-3 ">
+        <div class="card-header my-4 mx-2">
             <h2 class="card-title text-light">${member.name }</h2>
-            <h3 class="card-title">${member.getRole()}</h3>
+            <h3 class="card-title" id="manager">${member.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
@@ -86,17 +86,17 @@ const managerHTML = (member) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Team</title>
+    <title>Team Builder</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" />
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="src/style.css">
     </head>
     
     <body>
     <header>
-      <div class="container flex-row justify-space-between align-center py-4 bg-dark">
-        <h1 class="page-title text-light bg-primary py-4 px-3 text-center">My Team</h1>
+      <div class="container flex-row justify-space-between align-center py-4">
+        <h1 class="page-title py-4 px-3 text-center"> Team Builders</h1>
       </div>
     </header>
     <main class="row mx-4 my-4">
